6ea140f880d8df200a06b3ccac0d98aa614d3636
Abuddabi <web.abudabi@gmail.com>
2024-05-21 14:26:06 -0500

    refactoring and workflow

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,18 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+  base: "/music_library/"
 
-  build: {
-    outDir: "../dist",
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "src/index.html"),
-        cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        listing: resolve(__dirname, "src/product-listing/index.html"),
-      },
-    },
-  },
-});
+  // build: {
+  //   outDir: "../dist",
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(__dirname, "src/index.html"),
+  //       cart: resolve(__dirname, "src/cart/index.html"),
+  //       checkout: resolve(__dirname, "src/checkout/index.html"),
+  //       product: resolve(__dirname, "src/product_pages/index.html"),
+  //       listing: resolve(__dirname, "src/product-listing/index.html"),
+  //     },
+  //   },
+  // },
+}); 
