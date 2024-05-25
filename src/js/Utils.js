9da81d08ccb8c6b6c266b550b52c51bc8d98e297
Abuddabi <web.abudabi@gmail.com>
2024-05-25 02:12:46 -0500

    modals

--- src/js/Utils.js
+++ src/js/Utils.js
@@ -1,6 +1,6 @@
 export const fetchAPI = async (endpoint, params = {}, dev = false) => {
-  // const API_HOST = import.meta.env.VITE_API_HOST;
-  const API_HOST = "shazam.p.rapidapi.com";
+  const API_HOST = import.meta.env.VITE_PROXY_HOST;
+  // const API_HOST = "shazam.p.rapidapi.com";
   let url = new URL(`https://${API_HOST}${endpoint}`);
 
   for (let name in params) {
@@ -32,14 +32,14 @@ export const fetchAPI = async (endpoint, params = {}, dev = false) => {
 export const qs = (selector, parent = document) => parent.querySelector(selector);
 
 // set a listener for both touchend and click
-export function setClick(selector, callback) {
+export function setClick(selector, callback, options = {}) {
   if (typeof selector === "string") selector = qs(selector);
 
-  selector.addEventListener("touchend", (event) => {
-    event.preventDefault();
-    callback();
-  });
-  selector.addEventListener("click", callback);
+  selector.addEventListener("touchend", (e) => {
+    e.preventDefault();
+    callback(e);
+  }, options);
+  selector.addEventListener("click", (e) => callback(e), options);
 }
 
 // get params from url
@@ -50,4 +50,57 @@ export function getParams(param) {
   return urlParams.get(param);
 }
 
+export function handleCustomAPIKey() {
+  const body = qs("body");
+  const input = qs(".custom-API-input");
+
+  setClick("#custom-API-KEY-btn", () => {
+    body.classList.add("modal-active");
+    input.focus();
+  });
+  setClick(".modal-overlay", (e) => {
+    e.stopPropagation();
+    body.classList.remove("modal-active");
+  });
+  setClick(".custom-API-input-confirm", confirm);
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      confirm();
+    }
+  });
+
+  function confirm() {
+    const value = input.value;
+    if (!value) return;
+
+    setLocalStorage("custom-shazam-API", value);
+    input.value = "";
+
+    qs(".custom-API-text").innerHTML = "You are using your own API Key. Requests are faster.";
+    qs("#custom-API-KEY-btn").innerHTML = "Update API Key";
+
+    const newModal = document.createElement("div");
+    newModal.classList.add("modal", "modal-thank-you");
+    newModal.innerHTML = `<h3 class="modal-title">Thank You!</h3>`;
+    qs("main").append(newModal);
+
+    setClick(".modal-overlay", () => {
+      qs(".modal").style.display = "none";
+      setTimeout(() => {
+        qs(".modal").style.display = "block";
+        qs(".modal-thank-you").remove();
+      }, 1000);
+    }, { once: true });
+  }
+}
+
 export const noImage = "/music_library/images/no-avatar.webp";
+
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
-import "../css/style.css"
-import { qs } from "./Utils";
+import { qs, handleCustomAPIKey } from "./Utils";
 import { renderDetails } from "./Details";
 import { handleSearch } from "./Search";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  // fetch("https://shazam-api-proxy.onrender.com/bbbbbb?term=dark")
+  //   .then(res => res.json())
+  //   .then(res => console.log(res))
+  //   .catch(err => console.error('Error: ', err));
+
   if (qs("#search-form")) handleSearch();
 
   if (qs(".detail-page")) renderDetails();
+
+  if (qs("#custom-API-KEY-btn")) handleCustomAPIKey();
 });
